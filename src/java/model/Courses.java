1620750a98efeac7580eb15c67010c769e275410
Jiawen Xu <jiawen-xu@uiowa.edu>
2015-10-10 23:52:59 -0500

    added model, dbConn.properties, and ojdbc6.jar driver

---
+++ src/java/model/Courses.java
@@ -0,0 +1,66 @@
+
+package model;
+
+
+
+
+public class Courses {
+    
+    private int courseid;
+    private String coursename;
+    private String semester;
+    private int credit;
+   
+    public Courses() {
+        this.courseid = 0;
+        this.coursename = "";
+        this.semester = "";
+        this.credit = 0;
+    }
+    
+    public Courses(int courseid, String coursename, String semester, int credit) {
+        this.courseid = courseid;
+        this.coursename = coursename;
+        this.semester = semester;
+        this.credit = credit;
+    }
+
+    public int getCourseid() {
+        return courseid;
+    }
+
+    public void setCourseid(int courseid) {
+        this.courseid = courseid;
+    }
+
+    public String getCoursename() {
+        return coursename;
+    }
+
+    public void setCoursename(String coursename) {
+        this.coursename = coursename;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "Courses{" + "courseid=" + courseid + ", coursename=" + coursename + ", semester=" + semester + ", credit=" + credit + '}';
+    }
+
+    
+}
